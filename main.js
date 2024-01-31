cea3f2bd370aea1cbb1ac8cacdfbed7fdc7a8454
Pranish <pranishpoudel01@gmail.com>
2024-01-31 22:26:30 +0545

    feat: :sparkles: Tuple in TS

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
-var nameUser = "Lindas";
-console.log(nameUser.toUpperCase);
+const nowResponse= async() => {
+ const fetchNow= await fetch('https://randomuser.me/api/1.2/?nat=us')
+ console.log( await fetchNow.json());
+}
+
+
+nowResponse();
--- myUnion.ts
+++ myUnion.ts
@@ -1,17 +1,20 @@
-let score:number|string=33
+// Defining a union type
+type MyType = number | string;
 
-
-
-
-
-
-function isanan(user:Admin){
-    return 0
+// Function that takes a parameter of type MyType
+function displayData(data: MyType) {
+    console.log(data);
 }
 
+// Examples of using the function with different types
+displayData(10);       // Output: 10
+displayData("Hello");  // Output: Hello
+
+// Error: Argument of type 'boolean' is not assignable to parameter of type 'MyType'.
+// displayData(true);
 
 
 
 
-
+const Rbg : [number,number,number]= [234,234,423];
 
